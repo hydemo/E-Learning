46507e163718df70f751523ff7f44f20f40d4eca
alaric.ouyang <alaric.ouyang@ringcentral.com>
2024-03-21 23:33:51 +0800

    修复bug

--- E-Learning-api/src/auth/auth.module.ts
+++ E-Learning-api/src/auth/auth.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AdminModule } from 'src/module/admin/admin.module';
-import { UserModule } from 'src/module/user/user.module';
 
 import { AuthService } from './auth.service';
 import { AuthStrategy } from './auth.strategy';
 
 @Module({
   providers: [AuthService, AuthStrategy],
-  imports: [UserModule, AdminModule],
+  imports: [AdminModule],
 })
 export class AuthModule {}
